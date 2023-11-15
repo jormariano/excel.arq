9fe470d8db96f930f022d7ec5089aa21dbc65fd2
Jorgelina Mariano <jorgelina.mariano@gmail.com>
2023-11-15 20:16:56 +0100

    close-button-2

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -11,28 +11,42 @@ const Cart = ({ open, onClose }) => {
 
     if (!open) return null;
 
+    const handleModalClick = (e) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div className="overlay" onClick={onClose}>
-            <div onClick={(e) => {
-                e.stopPropagation();
-            }} ></div>
-            <h4 className="closeButton" onClick={(e) => { e.stopPropagation(); onClose(); }}> X </h4>
-            {cantidadTotal === 0 ? (
-                <div className="emptyCartMessage">
-                    <h3>No hay productos en el carrito.</h3>
-                    <h2><Link to="talleres" spy={true} smooth={true} offset={-90} duration={500} onClick={onClose}>Ver Productos</Link></h2>
-                </div>
-            ) : (
-                <div className='cart'>
-                    {carrito.map((producto, index) => <CartItem key={index} {...producto} />)}
-                    <h3>Total: {total}</h3>
-                    <h3>Cantidad total: {cantidadTotal}</h3>
-                    <div className='cart-button'>
-                        <button onClick={() => vaciarCarrito()}> Vaciar carrito </button>
-                        <button onClick={() => { /* Agregar la lógica de finalizar compra */ }}> Finalizar compra </button>
+        <div className="overlay" onClick={handleModalClick}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <h4 className="closeButton" onClick={onClose}> X </h4>
+                {cantidadTotal === 0 ? (
+                    <div className="emptyCartMessage">
+                        <h3>No hay productos en el carrito.</h3>
+                        <h2>
+                            <Link 
+                                to="talleres"
+                                spy={true}
+                                smooth={true}
+                                offset={-90}
+                                duration={500}
+                                onClick={onClose}
+                            >
+                                Ver Productos
+                            </Link>
+                        </h2>
                     </div>
-                </div>
-            )}
+                ) : (
+                    <div className='cart'>
+                        {carrito.map((producto, index) => <CartItem key={index} {...producto} />)}
+                        <h3>Total: {total}</h3>
+                        <h3>Cantidad total: {cantidadTotal}</h3>
+                        <div className='cart-button'>
+                            <button onClick={() => vaciarCarrito()}> Vaciar carrito </button>
+                            <button onClick={() => { /* Agregar la lógica de finalizar compra */ }}> Finalizar compra </button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
