775e4a455b66cbb25053955e1e851174eb7b580e
Jorgelina Mariano <jorgelina.mariano@gmail.com>
2023-11-15 19:39:11 +0100

    close-button

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -11,13 +11,12 @@ const Cart = ({ open, onClose }) => {
 
     if (!open) return null;
 
-
     return (
         <div className="overlay" onClick={onClose}>
             <div onClick={(e) => {
                 e.stopPropagation();
             }} ></div>
-            <h4 className="closeButton" onClick={() => { console.log('Cerrando modal'); onClose(); }}> X </h4>
+            <h4 className="closeButton" onClick={(e) => { e.stopPropagation(); onClose(); }}> X </h4>
             {cantidadTotal === 0 ? (
                 <div className="emptyCartMessage">
                     <h3>No hay productos en el carrito.</h3>
@@ -30,7 +29,7 @@ const Cart = ({ open, onClose }) => {
                     <h3>Cantidad total: {cantidadTotal}</h3>
                     <div className='cart-button'>
                         <button onClick={() => vaciarCarrito()}> Vaciar carrito </button>
-                        <button> Finalizar compra </button>
+                        <button onClick={() => { /* Agregar la lógica de finalizar compra */ }}> Finalizar compra </button>
                     </div>
                 </div>
             )}
